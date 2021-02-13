8a910c4312dcf12f09b4f59eee25a0284e9f7a73
mcolo <max.miracolo@gmail.com>
2021-02-13 17:34:20 -0500

    25min cron

--- keepAlive.js
+++ keepAlive.js
@@ -2,7 +2,7 @@ const cronJob = require("cron").CronJob;
 const axios = require("axios").default;
 
 (() => {
-  const job = new cronJob("0 */1 * * * *", () => {
+  const job = new cronJob("0 */20 * * * *", () => {
     axios
       .get("https://fathomless-reaches-08772.herokuapp.com/keepalive")
       .then((res) => {})
