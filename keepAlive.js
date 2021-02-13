fcc382ccd5213a2c131cf5e60b9ef7f6fbc7e423
mcolo <max.miracolo@gmail.com>
2021-02-13 17:09:02 -0500

    add cron

---
+++ keepAlive.js
@@ -0,0 +1,13 @@
+const cron = require("cron");
+const axios = require("axios").default;
+
+// globals
+const url = "https://fathomless-reaches-08772.heroku.app/keepalive";
+
+(() => {
+  const cronJob = cron.CronJob("0 * * ? * *", () => {
+    axios(url).then((res) => {});
+  });
+
+  cronJob.start();
+})();
